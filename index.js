e76e32acede880827e7a25d5bfb00db8dee20806
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 21:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return str[0].toUpperCase() + str.slice(1);
   }
   static sanitize(str){
-    return str.replace(/[^-,'A-Za-z0-9]+/g, '');
+    return str.replace(/[^-'\sA-Za-z0-9]+/g, '');
   }
   static titleize(str){
     return str.replace(/[^-,'A-Za-z0-9]+/g, '');
