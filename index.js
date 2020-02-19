452f8fecb758c5c219666c903da8f1079f216ca2
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 21:14:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
   static capitalize(str){
-    return str[0].uppercase + str.slice(1);
+    return str[0].toUpperCase() + str.slice(1);
   }
   static sanitize(str){
     return str.replace(/[^-,'A-Za-z0-9]+/g, '');
