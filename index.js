4652411b70e76390847f27ede4a58c171b90e5fb
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 21:40:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 class Formatter {
   //add static methods here
   static capitalize(str){
-    return str[0].uppercase + str.slice(1);
+    return str[0].toUpperCase() + str.slice(1);
   }
   static sanitize(str){
-    return str.replace(/[^-,'A-Za-z0-9]+/g, '');
+    return str.replace(/[^-'\sA-Za-z0-9]+/g, '');
   }
   static titleize(str){
-    return str.replace(/[^-,'A-Za-z0-9]+/g, '');
+    const notWanted=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    const words= str.split(" ");
+    const cleanedWords = words.map(word => notWanted.includes(word) ? word : Formatter.capitalize(word) )
+    // const capital = cleanedWords.map(word => Formatter.capitalize(word))
+    return [Formatter.capitalize(words[0]),...cleanedWords.slice(1)].join(" ");
   }
 }
